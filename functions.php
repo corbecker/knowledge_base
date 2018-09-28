8e83c4dee94b2f6c2e0f2405b91ad6f80061fb27
corbecker <corbinbecker@protonmail.com>
2018-09-28 15:48:50 -0700

    widgets implemented

--- functions.php
+++ functions.php
@@ -1,9 +1,11 @@
 <?php 
-// Register Custom Navigation Walker
+
+// Register Custom Navigation Walker for bootstrap menu dropdowns
 require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 
   //theme support
   function wpb_theme_setup(){
+    //add featured image functionality
     add_theme_support('post-thumbnails');
 
     //nav menus
@@ -14,6 +16,21 @@ require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
 
   //gets fired after the setup hook
   add_action('after_setup_theme', 'wpb_theme_setup');
+
+  //widget locations
+  function wpb_init_widgets($id){
+
+    register_sidebar(array(
+      'name'          => 'Sidebar',
+      'id'            => 'sidebar',
+      'before_widget' => '<div class="sidebar-module">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h4>',
+      'after_title'   => '</h4>'
+    ));
+  }
+
+  add_action('widgets_init', 'wpb_init_widgets');
 
 ?>
  
